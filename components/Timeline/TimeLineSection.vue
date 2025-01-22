f64e0644d6f99eb979810fb3a9840baa3571c3be
lucas-ipsum <s0572357@htw-berlin.de>
2025-01-22 11:20:08 +0100

    added initial setup for timeline

---
+++ components/Timeline/TimeLineSection.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+     TimeLube   
+    </div>
+</template>
+
+<script setup lang="ts">
+    
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="justify-center flex">
-        <div class="xl:max-w-[56rem]">
+        <div class="flex flex-col gap-10 xl:max-w-[56rem]">
             <HeroSection />
+            <TimeLineSection />
             <SkillSection />
         </div>
     </div>
@@ -10,4 +11,5 @@
 
 <script lang="ts" setup>
 import SkillSection from '~/components/Skills/SkillSection.vue';
+import TimeLineSection from '~/components/Timeline/TimeLineSection.vue';
 </script>
