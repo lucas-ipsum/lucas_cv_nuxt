5f8b191b9dc7fbaf6e6405e8c94d6a94dee10727
lucas-ipsum <s0572357@htw-berlin.de>
2025-01-31 19:35:33 +0100

    increased scale on social icons

--- components/Hero/SocialsSection.vue
+++ components/Hero/SocialsSection.vue
@@ -2,17 +2,17 @@
   <div class="w-full flex justify-center">
     <div class="flex w-[80%] sm:w-[300px] justify-evenly text-primary">
       <font-awesome
-        class="hover:scale-110 cursor-pointer"
+        class="hover:scale-125 cursor-pointer"
         :icon="['fab', 'linkedin']"
         size="2xl"
       ></font-awesome>
       <font-awesome
-        class="hover:scale-110 cursor-pointer"
+        class="hover:scale-125 cursor-pointer"
         :icon="['fab', 'square-github']"
         size="2xl"
       ></font-awesome>
       <font-awesome
-        class="hover:scale-110 cursor-pointer"
+        class="hover:scale-125 cursor-pointer"
         :icon="['fas', 'envelope']"
         size="2xl"
       ></font-awesome>
