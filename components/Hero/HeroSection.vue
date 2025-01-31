d283427e9e6242c472f63f552924b426797dc4d3
lucas-ipsum <s0572357@htw-berlin.de>
2025-01-31 19:33:58 +0100

    added social section (linkedin, github, mail) (wip)

--- components/Hero/HeroSection.vue
+++ components/Hero/HeroSection.vue
@@ -3,8 +3,12 @@
         <HeroImage />
         <HeroText />
     </div>
+    <SocialsSection />
+
 </template>
 
 <script setup lang="ts">
+import SocialsSection from './SocialsSection.vue';
+
     
 </script>
---
+++ components/Hero/SocialsSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="w-full flex justify-center">
+    <div class="flex w-[80%] sm:w-[300px] justify-evenly text-primary">
+      <font-awesome
+        class="hover:scale-110 cursor-pointer"
+        :icon="['fab', 'linkedin']"
+        size="2xl"
+      ></font-awesome>
+      <font-awesome
+        class="hover:scale-110 cursor-pointer"
+        :icon="['fab', 'square-github']"
+        size="2xl"
+      ></font-awesome>
+      <font-awesome
+        class="hover:scale-110 cursor-pointer"
+        :icon="['fas', 'envelope']"
+        size="2xl"
+      ></font-awesome>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
