ee5ab243cad14f3efafb60a5f4e5bb2c475e7be0
lucas-ipsum <s0572357@htw-berlin.de>
2025-09-03 16:25:28 +0200

    optimized sizing and postion for logo on small devices

--- components/Hero/HeroImage.vue
+++ components/Hero/HeroImage.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>
-        <img src="/static/images/logo_dev_tree.png" alt="Environmental Web-Developer Logo">
-    </div>
+  <div class="w-full flex justify-center mt-6 sm:mt-0">
+    <img
+      src="/static/images/logo_dev_tree.png"
+      width="auto"
+      class="h-16 sm:h-36"
+      alt="Environmental Web-Developer Logo"
+    />
+  </div>
 </template>
